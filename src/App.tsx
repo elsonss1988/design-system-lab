5b2527e9085f9124ce9054ec17b032bef0487a64
Elson Santos <elson.de.souza.santos1@ibm.com>
2022-10-15 22:03:23 -0300

    Adicionando StoryBook

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './styles/global.css'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <>
+    <h1 className="font-bold text-5xl text-violet-500">Hello World</h1>
+    <button className="bg-violet-500 font-medium 
+    px-4 py-3 rounded text-white hover:bg-violet-600">
+       Enviar
+    </button>
+  
+  </>
+  )
+}
+
